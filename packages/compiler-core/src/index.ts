5a2f92ab62582d3818cad4c79bfa9711f48b44c0
dduohereshui <2245675739@qq.com>
2022-07-11 23:55:32 +0800

    feat: 实现标签文本属性编译

--- packages/compiler-core/src/index.ts
+++ packages/compiler-core/src/index.ts
@@ -44,6 +44,7 @@ function parseTextData(context, endIndex) {
 }
 function getSelection(context, start, end?) {
   end = end || getCursor(context);
+
   return {
     start,
     end,
@@ -74,18 +75,141 @@ function parseText(context) {
 }
 function isEnd(context) {
   const source = context.source;
+  if (context.source.startsWith("</")) {
+    return true;
+  }
   // 字符串为空串就解析完成
   return source.length === 0;
 }
-function parse(template) {
-  // return template;
-  const context = createParserContext(template);
+function parseInterpolation(context) {
+  const start = getCursor(context);
+  const closeIndex = context.source.indexOf("}}", "{{".length);
+  advanceBy(context, 2);
+
+  const innerStart = getCursor(context);
+  const innerEnd = getCursor(context);
+
+  const rawContentLength = closeIndex - 2;
+
+  let preContent = parseTextData(context, rawContentLength);
+  let content = preContent.trim();
+  let startOffset = preContent.indexOf(content);
+  if (startOffset > 0) {
+    // 说明双大括号文本之前有空格
+    advancePositionWithMutation(innerStart, preContent, startOffset);
+  }
+  let endOffset = startOffset + content.length;
+  advancePositionWithMutation(innerEnd, preContent, endOffset);
+
+  advanceBy(context, 2);
+
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content,
+      loc: getSelection(context, innerStart, innerEnd),
+    },
+    loc: getSelection(context, start),
+  };
+}
+function advanceBySpaces(context) {
+  // 空格
+  const match = /^[ \t\r\n]+/.exec(context.source);
+  if (match) {
+    // 有空格
+    advanceBy(context, match[0].length);
+  }
+}
+function parseAttributeValue(context) {
+  const start = getCursor(context);
+  const quote = context.source[0];
+  let content;
+  if (quote === '"' || quote === "'") {
+    advanceBy(context, 1);
+    // 找到结束的引号
+    const endIndex = context.source.indexOf(quote);
+    content = parseTextData(context, endIndex);
+    advanceBy(context, 1);
+  }
+  return {
+    content,
+    loc: getSelection(context, start),
+  };
+}
+function parseAttribute(context) {
+  const start = getCursor(context);
+  const match = /^[^\t\r\n\f />][^\t\r\n\f />=]*/.exec(context.source);
+  let name = match[0];
+  advanceBy(context, name.length);
+  advanceBySpaces(context);
+  advanceBy(context, 1);
+  // "" ''
+  let value = parseAttributeValue(context);
+  const loc = getSelection(context, start);
+  // debugger;
+  return {
+    type: NodeTypes.ATTRIBUTE,
+    name,
+    value: {
+      type: NodeTypes.TEXT,
+      // content: value.content,
+      ...value,
+    },
+    loc,
+  };
+}
+function parseAttributes(context) {
+  const props = [];
+  while (context.source.length > 0 && !context.source.startsWith(">")) {
+    const prop = parseAttribute(context);
+    props.push(prop);
+    advanceBySpaces(context);
+  }
+  return props;
+}
+function parseTag(context) {
+  const start = getCursor(context);
+  const match = /^<\/?([a-z][^ \t\r\n/>]*)/.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBySpaces(context);
+
+  let props = parseAttributes(context);
+  //  自闭和标签
+  let isSelfClosing = context.source.startsWith("/>");
+  advanceBy(context, isSelfClosing ? 2 : 1);
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+    isSelfClosing,
+    props,
+    children: [],
+    loc: getSelection(context, start),
+  };
+}
+function parseElement(context) {
+  // 解析标签 <abc /> <aaa a='1'></aaa>
+  let ele = parseTag(context);
+  let children = parseChildren(context);
+  if (context.source.startsWith("</")) {
+    parseTag(context);
+  }
+  ele.children = children;
+  ele.loc = getSelection(context, ele.loc.start);
+  return ele;
+}
+function parseChildren(context) {
   const nodes = [];
   while (!isEnd(context)) {
     let node;
     const source = context.source;
+    // 双大括号表达式
     if (source.startsWith("{{")) {
+      node = parseInterpolation(context);
     } else if (source[0] === "<") {
+      // 标签
+      node = parseElement(context);
     }
     // 文本
     if (!node) {
@@ -93,9 +217,14 @@ function parse(template) {
     }
 
     nodes.push(node);
-    break;
+    // break;
   }
-  console.log(nodes);
+  return nodes;
+}
+function parse(template) {
+  // return template;
+  const context = createParserContext(template);
+  return parseChildren(context);
 }
 export function compile(template) {
   const ast = parse(template);
